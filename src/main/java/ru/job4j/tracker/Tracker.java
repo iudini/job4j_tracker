94c3d755d3cef8b9c5080180d1f103c715ae1186
iudini <hoiitzx@gmail.com>
2020-09-28 13:04:05 +0300

    1. Чтение кода и базовые ошибки [#364710]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,18 +5,18 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
-    public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
+    public Item add(Item task) {
+        task.setId(ids++);
+        items[size++] = task;
+        return task;
     }
 
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            Item task = items[index];
+            if (task.getId() == id) {
+                rsl = task;
                 break;
             }
         }
