cbdf9676192bb46d489db62d578b6c4fc1579fd4
iudini <hoiitzx@gmail.com>
2020-09-29 14:26:48 +0300

    удалил папку oop

--- src/main/java/ru/job4j/oop/DummyDic.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.oop;
-
-public class DummyDic {
-    public String engToRus(String eng) {
-        String unknownWord = "Неизвестное слово" + eng;
-        return unknownWord;
-    }
-}
