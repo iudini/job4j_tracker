ded6caa32c94bfe9967d1cb6c68018df4614c5cf
iudini <hoiitzx@gmail.com>
2020-10-01 11:32:38 +0300

    7. toString [#364748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,9 +5,10 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item created = new Item();
+        Item created = new Item(0, "start");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTime = created.getCreated().format(formatter);
         System.out.println(currentDateTime);
+        System.out.println(created);
     }
 }
