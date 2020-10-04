474df04ac8eb45b7d338b981df57e62710c4fa93
iudini <hoiitzx@gmail.com>
2020-10-04 13:37:06 +0300

    1.1. Тесты на StartUI. [#364705]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,6 +9,28 @@ import static org.junit.Assert.*;
 public class StartUITest {
 
     @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
+
+    @Test
     public void whenCreateItem() {
         Output out = new StubOutput();
         Input in = new StubInput(
@@ -73,13 +95,13 @@ public class StartUITest {
         assertThat(out.toString(), is(
                 "Menu." + System.lineSeparator() +
                         "0. Show all Item" + System.lineSeparator() +
-                        "1. Exit Program" + System.lineSeparator() +
+                        "1. Exit" + System.lineSeparator() +
                         "=== Show all Item ====" + System.lineSeparator() +
                         "id 1 - First item" + System.lineSeparator() +
                         "id 2 - Second item" + System.lineSeparator() +
                         "Menu." + System.lineSeparator() +
                         "0. Show all Item" + System.lineSeparator() +
-                        "1. Exit Program" + System.lineSeparator()
+                        "1. Exit" + System.lineSeparator()
         ));
     }
 
@@ -99,12 +121,12 @@ public class StartUITest {
         assertThat(out.toString(), is(
                 "Menu." + System.lineSeparator() +
                         "0. Find Item by Id" + System.lineSeparator() +
-                        "1. Exit Program" + System.lineSeparator() +
+                        "1. Exit" + System.lineSeparator() +
                         "=== Find Item by Id ====" + System.lineSeparator() +
                         "id 1 - First item" + System.lineSeparator() +
                         "Menu." + System.lineSeparator() +
                         "0. Find Item by Id" + System.lineSeparator() +
-                        "1. Exit Program" + System.lineSeparator()
+                        "1. Exit" + System.lineSeparator()
         ));
     }
 
@@ -125,13 +147,13 @@ public class StartUITest {
         assertThat(out.toString(), is(
                 "Menu." + System.lineSeparator() +
                         "0. Find items by name" + System.lineSeparator() +
-                        "1. Exit Program" + System.lineSeparator() +
+                        "1. Exit" + System.lineSeparator() +
                         "=== Find items by name ====" + System.lineSeparator() +
                         "id 1 - First item" + System.lineSeparator() +
                         "id 2 - First item" + System.lineSeparator() +
                         "Menu." + System.lineSeparator() +
                         "0. Find items by name" + System.lineSeparator() +
-                        "1. Exit Program" + System.lineSeparator()
+                        "1. Exit" + System.lineSeparator()
         ));
     }
 
@@ -148,7 +170,7 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
                 "Menu." + System.lineSeparator() +
-                        "0. Exit Program" + System.lineSeparator()
+                        "0. Exit" + System.lineSeparator()
         ));
     }
 }
