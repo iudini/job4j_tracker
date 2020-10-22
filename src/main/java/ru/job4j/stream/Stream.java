bfb2fa3fe2aa47db9c72da3fb301d4ab0acb1fff
iudini <hoiitzx@gmail.com>
2020-10-22 13:16:04 +0300

    0. Stream API [#364493]

---
+++ src/main/java/ru/job4j/stream/Stream.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>(List.of(-10, 7, 2, 3, 5, 13, 6));
+        List<Integer> list = array.stream().filter(x -> x > 0).collect(Collectors.toList());
+    }
+}
