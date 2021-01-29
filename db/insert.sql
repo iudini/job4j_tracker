c5860afb8ebb77695bca5949ef978505d0b1dda0
iudini <hoiitzx@gmail.com>
2021-01-29 23:38:41 +0300

    1. Sql схема для проекта Tracker. [#399904]

---
+++ db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO items(name) VALUES ('1 task'), ('2 task'), ('3 task'), ('4 task'), ('5 task'),
+                               ('6 task'), ('7 task'), ('8 task'), ('9 task'), ('10 task');
