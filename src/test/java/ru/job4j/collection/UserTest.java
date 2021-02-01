848674cf5ca8de1855aa55c8b9a7c7401f10999c
iudini <hoiitzx@gmail.com>
2021-02-01 13:37:26 +0300

    checkstyle

--- src/test/java/ru/job4j/collection/UserTest.java
+++ src/test/java/ru/job4j/collection/UserTest.java
@@ -2,8 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
@@ -12,7 +11,9 @@ import static org.junit.Assert.*;
 public class UserTest {
     @Test
     public void whenAsc() {
-        Set<User> users = Set.of(new User("Petr", 32), new User("Ivan", 31));
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
         Iterator<User> it = users.iterator();
         assertThat(it.next(), is(new User("Ivan", 31)));
         assertThat(it.next(), is(new User("Petr", 32)));
